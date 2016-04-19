b4c8022e9853c5ec80b129c6d7cc19607013183d
Phat Luc <phatluc1212@gmail.com>
2016-04-19 13:48:15 -0700

    completed function add

--- app/src/main/java/com/example/jake/unit_test/MainActivity.java
+++ app/src/main/java/com/example/jake/unit_test/MainActivity.java
@@ -8,9 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText text_1;
+    private EditText text_2;
+    private TextView sum_text;
+    //private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        text_1 = (EditText) findViewById(R.id.edit_1);
+        text_2 = (EditText) findViewById(R.id.edit_2);
+        sum_text = (TextView) findViewById(R.id.result_sum);
+
     }
 
     @Override
@@ -49,4 +62,21 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void sumBoxes(View v){
+        Double sum = 0.0;
+        Scanner scan_1 = new Scanner(text_1.getText().toString());
+        Scanner scan_2 = new Scanner(text_2.getText().toString());
+        try {
+            Double num_1 = scan_1.nextDouble();
+            Double num_2 = scan_2.nextDouble();
+            sum = num_1 + num_2;
+        }
+        catch(Exception e) {
+            sum_text.setText("Invalid input. Please try again!");
+            return;
+        }
+        sum_text.setText(sum.toString());
+    }
+
 }
